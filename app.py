42601ea6dfd9ac46ddc2a6124587eb4d743dc7cf
darisdzakwanhoesien2 <48860799+darisdzakwanhoesien2@users.noreply.github.com>
2025-03-30 18:55:08 +0700

    Stage 1 in Deliverable 4

--- app.py
+++ app.py
@@ -2,28 +2,29 @@
 import os
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flasgger import Swagger
 from extensions import db
 from journalapi.api import api_bp
 from journalapi.cli import init_db_command
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+
     app.config.from_mapping(
         SECRET_KEY="dev",
         SQLALCHEMY_DATABASE_URI="sqlite:///" + os.path.join(app.instance_path, "journal.db"),
-        SQLALCHEMY_TRACK_MODIFICATIONS=False
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        SWAGGER={"title": "PWP Journal API", "uiversion": 3}
     )
 
     if test_config:
         app.config.update(test_config)
 
-    try:
-        os.makedirs(app.instance_path, exist_ok=True)
-    except OSError:
-        pass
+    os.makedirs(app.instance_path, exist_ok=True)
 
     db.init_app(app)
     JWTManager(app)
+    Swagger(app, template_file="docs/openapi.yaml")  # <- Load your YAML here
 
     app.register_blueprint(api_bp)
     app.cli.add_command(init_db_command)
